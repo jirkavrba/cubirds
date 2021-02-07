9d10a9ebb022b599ec47be005e64497c7a90f31f
Jiří Vrba <jiri@vrba.dev>
2021-02-07 23:10:56 +0100

    Initial commit

---
+++ src/main/java/dev/vrba/cubirds/CubirdsApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.cubirds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubirdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubirdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vrba/cubirds/CubirdsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vrba.cubirds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubirdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
